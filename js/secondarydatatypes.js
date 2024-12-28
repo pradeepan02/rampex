87099b02d8cfea44b8ce91190e315fca7f398e74
pradeep <pradeep02012005@gmail.com>
2024-12-28 10:19:03 +0530

    secondarydatatype.js

---
+++ js/secondarydatatypes.js
@@ -0,0 +1,25 @@
+
+// array
+a=[10,20,20]
+console.log(a);
+b=[10,20,[11,22,33]]
+console.log(b);
+c=["aa","ddd","aaa"]
+console.log(c);
+d=["aa","bb","cc",["a","c"]]
+console.log(d);
+
+
+//object
+o1={"fname":"hari","age":23}
+console.log(o1);
+o2={}
+console.log(o2);
+o2["movie"]="GOAT"
+o2["rating"]="Block Buster"
+console.log(o2);
+console.log(o2["movie"])
+console.log(o2.movie)
+o3={}
+o3["movie"]="Leo"
+o3["rating"]="Super Block Buster"
