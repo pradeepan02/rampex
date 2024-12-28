795e06c9173e77a1c150fd57e4a86a1921d09302
pradeep <pradeep02012005@gmail.com>
2024-12-28 10:31:08 +0530

    looping.js

--- js/looping.js
+++ js/looping.js
@@ -8,3 +8,21 @@ a=[10,20,30,"hello","world"]
 for (const key in a) {
     console.log("index of",a[key],"is",key);    
 }
+for(let i=0;i<5;i++){
+    console.log(i);
+    
+}
+l=0;
+while(l<5){
+    console.log(l);
+    l++;
+    
+}
+do{
+    console.log(l);
+    
+}
+while(l<5){
+    console.log(l);
+
+}
