0feda761d5c88a297b9986df5148f89a1f693070
pradeep <pradeep02012005@gmail.com>
2024-12-28 11:15:26 +0530

    operator.js

---
+++ js/operators.js
@@ -0,0 +1,66 @@
+
+//assignment operator
+let a=5;
+let b=6;
+//arithmetic operator
+console.log("Add",a+b);
+console.log("sub",a-b);
+console.log("mul",a*b);
+console.log("div",a/b);
+console.log("mod",a%b);
+//unary operator
+console.log("pre increment",a);
+console.log("pre increment",++a);
+console.log("post increment",a++);
+console.log("post increment",a);
+console.log("pre decrement",a);
+console.log("pre decrement",--a);
+console.log("post decrement",a--);
+console.log("post decrement",a);
+//conditional or relational
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a===b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a!==b);
+//bitwise
+console.log(a&b);
+console.log(a|b);
+console.log(~a);
+console.log(a^b);
+console.log(a>>b);
+console.log(a<<b);
+console.log(a>>>b);
+//logical
+console.log(a==b&&b==a);
+console.log(a==b||b==a);
+console.log(!(a==b));
+//ternary operator
+let num=10;
+console.log((num%2==0)?"even":"odd");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
